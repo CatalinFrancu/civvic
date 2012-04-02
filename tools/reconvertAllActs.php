dd299d90a8044f1333e74a40ae5113b9d4ebbba1
cata <cata@d440b0c1-a408-4afe-a3bf-7e427467e8b7>
2012-04-02 09:44:50 +0000

    Show diffs for reconverted acts.

--- tools/reconvertAllActs.php
+++ tools/reconvertAllActs.php
@@ -2,12 +2,25 @@
 
 require_once __DIR__ . '/../lib/Util.php';
 
-$actVersions = Model::factory('ActVersion')->find_many();
+$count = Model::factory('ActVersion')->count();
+print "Reconverting $count act versions\n";
+
+$actVersions = Model::factory('ActVersion')->order_by_asc('id')->find_many();
 
 foreach ($actVersions as $av) {
   print("Saving act version {$av->id}\n");
   $av->contents = StringUtil::cleanupUserInput($av->contents); // Force dirty field
+  $oldHtml = $av->htmlContents;
   $av->save();
+  if ($av->htmlContents != $oldHtml) {
+    file_put_contents('/tmp/civvic_old.html', $oldHtml);
+    file_put_contents('/tmp/civvic_new.html', $av->htmlContents);
+    printf("Act http://civvic.ro/act?id=%d version %d has changed, diff follows\n", $av->actId, $av->versionNumber);
+    $output = array();
+    exec('diff /tmp/civvic_old.html /tmp/civvic_new.html', $output);
+    print implode("\n", $output);
+    print "\n\n";
+  }
 }
 
 ?>
