b7a8fb08bd2ae02c8b092bac42968e1862dcb3b4
cata <cata@d440b0c1-a408-4afe-a3bf-7e427467e8b7>
2012-04-06 16:31:42 +0000

    Improve ordering of monitors.

--- www/monitoare.php
+++ www/monitoare.php
@@ -3,7 +3,7 @@
 require_once '../lib/Util.php';
 
 $yearMap = array();
-$monitors = Model::factory('Monitor')->raw_query('select * from monitor order by cast(number as unsigned)', null)->find_many();
+$monitors = Model::factory('Monitor')->raw_query('select * from monitor order by cast(number as unsigned), number', null)->find_many();
 
 foreach ($monitors as $m) {
   if (!array_key_exists($m->year, $yearMap)) {
