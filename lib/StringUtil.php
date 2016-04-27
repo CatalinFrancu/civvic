1743bdd99cd78f34b25f6bbe05a96c02d797efb5
cata <cata@d440b0c1-a408-4afe-a3bf-7e427467e8b7>
2016-04-27 07:51:01 +0000

    Add old code from cipi.

--- lib/StringUtil.php
+++ lib/StringUtil.php
@@ -4,6 +4,8 @@ class StringUtil {
   static $months = array('ianuarie', 'februarie', 'martie', 'aprilie', 'mai', 'iunie',
                          'iulie', 'august', 'septembrie', 'octombrie', 'noiembrie', 'decembrie');
   static $monthsRoman = array('i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x', 'xi', 'xii');
+  // Useful because Ubuntu + PHP + locales = idiot party
+  static $daysOfWeek = array('duminică', 'luni', 'marți', 'miercuri', 'joi', 'vineri', 'sâmbătă');
 
   static function startsWith($string, $substring) {
     $startString = substr($string, 0, strlen($substring));
@@ -84,6 +86,32 @@ class StringUtil {
 
   static function capitalize($s) {
     return mb_strtoupper(mb_substr($s, 0, 1)) . mb_substr($s, 1);
+  }
+
+  // convert Arabic numbers to Roman numerals
+  // Thanks to http://www.go4expert.com/forums/showthread.php?t=4948
+  function arabicToRoman($num) {
+    // Make sure that we only use the integer portion of the value
+    $n = intval($num);
+    $result = '';
+ 
+    // Declare a lookup array that we will use to traverse the number:
+    $lookup = array('M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400,
+                    'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40,
+                    'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1);
+ 
+    foreach ($lookup as $roman => $value) 
+      {
+        // Determine the number of matches
+        $matches = intval($n / $value);
+        // Store that many characters
+        $result .= str_repeat($roman, $matches);
+        // Substract that from the number
+        $n = $n % $value;
+      }
+ 
+    // The Roman numeral should be built, return it
+    return $result;
   }
 
   /** Splits
--- tools/extractPdf.php
+++ tools/extractPdf.php
@@ -19,32 +19,52 @@ if (count($argv) != 2) {
 
 $pdfFileName = realpath($argv[1]);
 
-exec(sprintf("pdftohtml -i -noframes -s %s %s", $argv[1], TMP_FILE));
+exec(sprintf("pdftohtml -nodrm -i -noframes -s %s %s", $argv[1], TMP_FILE));
 $html = file_get_contents(TMP_FILE);
 $html = preprocessHtml($html);
 $xml = simplexml_load_string($html);
-//$html = simplexml_load_file("/home/cata/Desktop/text.xml");
-
-traverse($xml->BODY);
-//traverse($html->tag1, false, false);
-//var_dump((string)$html->tag1->tag4);
+$text = traverse($xml->BODY);
+$text = postprocess($text);
+print($text);
 
 /**************************************************************************/
 
 function preprocessHtml($html) {
   $weirdEncoding = strpos($html, 'þ') !== false;
   if ($weirdEncoding) {
-    $html = str_replace(array('ã', 'Ã', 'º', 'ª', 'þ', 'Þ', 'Ñ', '”', 'Ò', '&#160;'),
-                        array('ă', 'Ă', 'ș', 'Ș', 'ț', 'Ț', '-', '„', '”', ' '), $html);
+    $html = str_replace(array('ã', 'Ã', 'º', 'ª', 'þ', 'Þ', 'Ñ', '”', 'Ò'),
+                        array('ă', 'Ă', 'ș', 'Ș', 'ț', 'Ț', '-', '„', '”'), $html);
   }
+  $html = str_replace(array('—', '&#160;'),
+                      array('-', ' '), $html);
   $html = preg_replace('/<\\/?b>/i', "'''", $html);
   $html = preg_replace('/<\\/?i>/i', "''", $html);
   $html = preg_replace('/<br\\/>/i', ' ', $html);
   $html = preg_replace('/<A name="outline">(.|\\n)+<hr>/', '', $html); // Remove the outline because it is malformed
+  $html = preg_replace("/<P[^>]+>MONITORUL OFICIAL AL ROMÂNIEI, PARTEA I, Nr\\. \\d+\\/\\d+\\.[IVX]+\\.\\d{4}<\\/P>\\s+" .
+                       "<P[^>]+>\\d+<\\/P>\\s+/", '', $html);
+
+  // Replace spaced-out titles
+  $matches = array();
+  preg_match_all("/((([A-Z ]|Ă|Â|Î|Ș|Ț) ){3,}([A-Z ]|Ă|Â|Î|Ș|Ț))/", $html, $matches, PREG_OFFSET_CAPTURE);
+  foreach (array_reverse($matches[0]) as $match) {
+    $text = $match[0];
+    $len = mb_strlen($text);
+    $pos = $match[1];
+
+    $result = '';
+    for ($i = 0; $i < $len; $i++) {
+      if ($i % 2 == 0) {
+        $result .= mb_substr($text, $i, 1);
+      }
+    }
+    $html = substr($html, 0, $pos) . $result . substr($html, $pos + strlen($text));
+  }
   return $html;
 }
 
 function traverse($node) {
+  $result = '';
   global $previousLeft, $previousTop;
 
   $name = strtolower($node->getName());
@@ -58,19 +78,27 @@ function traverse($node) {
   if (($top == $previousTop) ||
       ($top - $previousTop >= PARA_DY_MIN && $top - $previousTop <= PARA_DY_MAX &&
        $previousLeft - $left >= PARA_DX_MIN && $previousLeft - $left <= PARA_DX_MAX)) {
-    print ' ';
+    $result .= ' ';
   } else {
-    print "\n\n";
+    $result .= "\n\n";
   }
 
-  if (!skippableContents($contents)) {
-    print "$contents";
+  if (!skippableLine($contents)) {
+    $result .= "$contents";
   }
   $previousLeft = $left;
   $previousTop = $top;
+
+  $children = array();
+  // Collect the children
   foreach ($node->children() as $child) {
-    traverse($child);
+    $children[] = $child;
   }
+
+  foreach ($children as $child) {
+      $result .= traverse($child);
+  }
+  return $result;
 }
 
 /* Returns a map of key => value pairs. Splits the 'px' prefix on distances. */
@@ -96,6 +124,68 @@ function getOrNull($map, $key) {
   return array_key_exists($key, $map) ? $map[$key] : null;
 }
 
-function skippableLine($line)
+function skippableLine($line) {
+  if (!$line) {
+    return true;
+  }
+
+  if (stripos("produs electronic destinat exclusiv informării gratuite a persoanelor fizice " .
+              "asupra actelor ce se publică în monitorul oficial al româniei", $line) !== false) {
+    return true;
+  }
+
+  return false;
+}
+
+function postprocess($text) {
+  // Remove extra newlines
+  $text = preg_replace("/\\n{3,}/", "\n\n", $text);
+
+  // Extract the number and date from the technical box at the end.
+  $match = array();
+  $matchFound = preg_match("/Monitorul Oficial al României, Partea I, nr. (?P<number>[^\\/]+)\\/" .
+                           "(?P<day>\\d+)\\.(?P<month>[IVX]+)\\.(?P<year>\\d{4}) conține \\d+ pagini\\./",
+                           $text, $match);
+  $headerTemplate = "__FORCETOC__\n" .
+    "[[Category:Monitorul Oficial|*%s %04s]]\n" .
+    "\n" .
+    "= Monitorul Oficial al României =\n" .
+    "Anul %s, Nr. [[issue::%04s]] - Partea I - %s, %s %s [[year::%s]]\n";
+
+  if ($matchFound) {
+    $month = 1 + array_search(strtolower($match['month']), StringUtil::$monthsRoman);
+    $monthName = StringUtil::$months[$month - 1];
+    $timestamp = mktime(0, 0, 0, $month, $match['day'], $match['year']);
+    $dow = StringUtil::$daysOfWeek[date('w', $timestamp)];
+    $header = sprintf($headerTemplate,
+                      $match['year'], $match['number'], StringUtil::arabicToRoman($match['year'] - 1988),
+                      $match['number'], StringUtil::capitalize($dow), $match['day'], $monthName, $match['year']
+                      );
+  } else {
+    $header = vsprintf($headerTemplate, array_fill(0, 8, '??'));
+  }
+
+  // Remove the summary and prepend the header
+  $sumStart = strpos($text, "\n'''SUMAR'''\n");
+  $actStart = strpos($text, "\n'''", $sumStart + 10);
+  if ($sumStart !== false && $actStart !== false) {
+    // $text = substr($text, $actStart); // Breaks for P2 of M.O. 201/2010
+  }
+  $text = $header . $text;
+
+  // Convert some known section titles
+  $text = str_replace("\n'''ACTE ALE COMISIEI DE SUPRAVEGHERE AASIGURĂRILOR'''\n",
+                      "\n== Acte ale Comisiei de Supraveghere a Asigurărilor ==\n", $text);
+  $text = str_replace("\n'''ACTE ALE ORGANELOR DE SPECIALITATE'''\n\n'''ALE ADMINISTRAȚIEI PUBLICE CENTRALE'''\n",
+                      "\n== Acte ale organelor de specialitate ale administrației publice centrale ==\n", $text);
+  $text = str_replace("\n'''DECIZII ALE CURȚII CONSTITUȚIONALE'''\n",
+                      "\n== Decizii ale Curții Constituționale ==\n", $text);
+  $text = str_replace("\n'''LEGI ȘI DECRETE'''\n",
+                      "\n== Legi și decrete ==\n", $text);
+  $text = str_replace("\n'''ORDONANȚE ALE GUVERNULUI ROMÂNIEI'''\n",
+                      "\n== Ordonanțe ale Guvernului României ==\n", $text);
+
+  return $text;
+}
 
 ?>
